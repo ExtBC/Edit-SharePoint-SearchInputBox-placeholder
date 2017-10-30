f25a332e95d1902ce80fac30fa22b41707f13d9b
WIN-D1CUL4JGSN8\Ivan <extbc.blog@gmail.com>
2017-10-30 15:37:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+﻿     $(document).ready(function(){
+          SetDefaultValues();
+          $('#ctl00_PlaceHolderSearchArea_SmallSearchInputBox1_csr_sbox')
+               .focus(FocusPlaceHolder);
+          $('#ctl00_PlaceHolderSearchArea_SmallSearchInputBox1_csr_sbox')
+               .blur(BlurPlaceHolder);
+     });
+
+     function FocusPlaceHolder(){
+          var value = $('#ctl00_PlaceHolderSearchArea_SmallSearchInputBox1_csr_sbox')
+               .val();
+          if(value == "Поиск") 
+          $('#ctl00_PlaceHolderSearchArea_SmallSearchInputBox1_csr_sbox')
+               .val("");
+     }
+
+     function BlurPlaceHolder(){
+          var value = $('#ctl00_PlaceHolderSearchArea_SmallSearchInputBox1_csr_sbox')
+               .val();
+          if(value == "Поиск" || value == "Поиск на этом сайте" )
+          SetDefaultValues(); 
+
+     }
+
+     function SetDefaultValues(){
+          $('#ctl00_PlaceHolderSearchArea_SmallSearchInputBox1_csr_sbox')
+               .attr("placeholder", "Поиск по сайту");
+          $('#ctl00_PlaceHolderSearchArea_SmallSearchInputBox1_csr_sbox')
+               .attr("title", "Поиск по сайту");
+          $('#ctl00_PlaceHolderSearchArea_SmallSearchInputBox1_csr_sbox')
+               .val("Поиск"); 
+     }
